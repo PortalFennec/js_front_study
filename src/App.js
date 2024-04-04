ea724ef152ad8a581d3a1a8003461a290e1c0340
PortalFennec <portal.foxian@gmail.com>
2024-04-04 14:53:23 +0300

    Multiple screens

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ToDoTask from './ToDoTask';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import ToDoList from './ToDoList';
 import ToDoTaskAdd from './ToDoTaskAdd';
 
 class App extends React.Component {
@@ -10,8 +12,10 @@ class App extends React.Component {
 			tasks: []
 		}
 		
-		this.onTaskDelete = this.onTaskDelete.bind(this);
 		this.onTaskAdd = this.onTaskAdd.bind(this);
+		this.onTaskDelete = this.onTaskDelete.bind(this);
+		
+		
 	}
 	
 	componentDidMount() {
@@ -41,16 +45,12 @@ class App extends React.Component {
 	render() {	
 		 return (
 			<div className="App">
-				<ToDoTaskAdd onTaskAdd={this.onTaskAdd} />
-				<ul>
-				{
-					this.state.tasks.map((task) => {
-						return (
-							<ToDoTask task={task} onTaskDelete={this.onTaskDelete} key={task._id} />
-						)
-					})			
-				}
-				</ul>
+				<Router>
+					<Routes>
+						<Route path="/" element={<ToDoList tasks={this.state.tasks} onTaskDelete={this.onTaskDelete} />} />
+						<Route path="/add" element={<ToDoTaskAdd onTaskAdd={this.onTaskAdd} />} />
+					</Routes>
+				</Router>
 			</div>
 		);
 	}
